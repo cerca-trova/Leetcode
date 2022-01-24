39c4cff65bee5a44cbbe9abe3cf13e38dca50091
cerca_tju <ce88808@gmail.com>
2022-01-24 19:57:03 +0800

    part solved palindrome problem

--- feature/validParentheses.go
+++ feature/validParentheses.go
@@ -1,15 +1,56 @@
 package feature
 
-import "fmt"
+//var ValidParenthesesAnsiNums = []int{40, 41, 123, 125, 91, 93}
+//var ValidParenthesesPairs = map[byte]byte{40: 41, 123: 125, 91: 93}
 
-var ValidParenthesesAnsiNums = []int{40, 41, 123, 125, 91, 93}
-
-func ValidParentheses(s string) {
+func ValidParentheses(s string) bool {
 	length := len(s)
-	if length%2 != 0 || s[0] == 41 || s[0] == 125 || s[0] == 93 {
-		fmt.Println(0)
+	if length%2 != 0 {
+		return false
+	} else if verifySymmetricalSlice(s) {
+		return true
 	} else {
-		givenParenthesesPairs := length / 2
+		//givenParenthesesPairs := length / 2
+		for i := 0; i < length; i += 2 {
+			if validParenthesesAnsiNums(s[i], s[i+1]) {
+				continue
+			} else {
+				return false
+			}
+		}
+		return true
 	}
 
+}
+func validParenthesesAnsiNums(firstBracketAnsiNum byte, lastBracketAnsiNum byte) bool {
+	if firstBracketAnsiNum == 40 && lastBracketAnsiNum == 41 {
+		return true
+	} else if firstBracketAnsiNum == 91 && lastBracketAnsiNum == 93 {
+		return true
+	} else if firstBracketAnsiNum == 123 && lastBracketAnsiNum == 125 {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func verifySymmetricalSlice(s string) bool {
+	middle_end := len(s) / 2
+	for i := 0; i < middle_end; i++ {
+		if !validParenthesesAnsiNums(s[i], s[len(s)-i-1]) {
+			return false
+		}
+	}
+	return true
+}
+
+func ifEachSegmentsValid(s string) bool {
+	var segment_part int = 1
+	for i := 0; i < len(s); i++ {
+		if s[i] == 40 || s[i] == 91 || s[i] == 123 {
+			segment_part++
+		}
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	functions "github.com/cerca-trova/Leetcode/feature"
 )
 
 func main() {
-	functions.ValidParentheses(")([")
+	var s = []int{1, 2, 3, 4}
+	s1 := s[0:3]
+	if functions.ValidParentheses("[()]") {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("NOT Correct!")
+	}
+	for i := 0; i < len(s1); i++ {
+		if s[i] == 1 {
+			i = 2
+
+		}
+		fmt.Println(s[i])
+		continue
+	}
+
 }
