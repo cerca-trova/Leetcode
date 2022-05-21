1424901aab1ea6fc4d8d33e5efe583543d115054
unknown <chenran2@sungrow.cn>
2022-05-21 14:51:39 +0800

    add parenthese sample

---
+++ constant/validParentheses.go
@@ -0,0 +1,5 @@
+package constant
+
+var ValidParenthesesSample1 string = "(){}"
+var ValidParenthesesSample2 string = "((()))"
+var ValidParenthesesSample3 string = "{}{()}"
--- main.go
+++ main.go
@@ -7,20 +7,9 @@ import (
 )
 
 func main() {
-	var s = []int{1, 2, 3, 4}
-	s1 := s[0:3]
-	if functions.ValidParentheses("[()]") {
+	if functions.ValidParentheses("[()][]") {
 		fmt.Println("Correct!")
 	} else {
-		fmt.Println("NOT Correct!")
+		fmt.Println("Not Correct!")
 	}
-	for i := 0; i < len(s1); i++ {
-		if s[i] == 1 {
-			i = 2
-
-		}
-		fmt.Println(s[i])
-		continue
-	}
-
 }
