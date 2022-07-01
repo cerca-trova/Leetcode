9f0458c11590a1abcbfdc1e5d7ba3c9272d2603a
unknown <chenran2@sungrow.cn>
2022-07-01 18:17:29 +0800

    add install hdfs script

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 
+	constants "github.com/cerca-trova/Leetcode/constant"
 	functions "github.com/cerca-trova/Leetcode/feature"
 )
 
 func main() {
-	if functions.ValidParentheses("[()][]") {
+	if functions.ValidParentheses(constants.ValidParenthesesSample3) {
 		fmt.Println("Correct!")
 	} else {
 		fmt.Println("Not Correct!")
